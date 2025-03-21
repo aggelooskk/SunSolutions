13991ad62aa1efdb8f608361137daf8d93881332
Aggelosk <akermezo@yahoo.com>
2025-03-21 12:53:55 +0200

    creating header component

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from "react";
 
 const Header = () => {
   return (
-    <div>
-      Header
-    </div>
-  )
-}
+    <>
+      <nav className="shadow-md p-4">
+        <div className="container mx-auto flex justify-between items-center">
+          <h1 className="text-2xl font-semibold text-gray-700">Sun Solutions</h1>
+          <ul className="flex space-x-6 text-gray-600">
+            <li className="hover:text-gray-800 cursor-pointer">Home</li>
+            <li className="hover:text-gray-800 cursor-pointer">Contact us</li>
+            <li className="hover:text-gray-800 cursor-pointer">About</li>
+          </ul>
+        </div>
+      </nav>
+    </>
+  );
+};
 
-export default Header
+export default Header;
