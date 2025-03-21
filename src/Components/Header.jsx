8bb0ce9016ba29e38cc9e1d98ac79846b48f83e3
Aggelosk <akermezo@yahoo.com>
2025-03-21 18:29:26 +0200

    rewrite header component,install react icons

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,19 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
+import { GrClose } from "react-icons/gr";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 const Header = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
   return (
-    <>
-      <nav className="shadow-md p-4">
-        <div className="container mx-auto flex justify-between items-center">
-          <h1 className="text-2xl font-semibold text-gray-700">Sun Solutions</h1>
-          <ul className="flex space-x-6 text-gray-600">
-            <li className="hover:text-gray-800 cursor-pointer">Home</li>
-            <li className="hover:text-gray-800 cursor-pointer">Contact us</li>
-            <li className="hover:text-gray-800 cursor-pointer">About</li>
-          </ul>
-        </div>
+    <header className="flex flex-row items-center justify-between sm:justify-around p-2">
+      <a
+        href="/"
+        className="flex items-center h-10 font-bold uppercase text-black hover:opacity-90"
+      >
+        Title
+      </a>
+      <nav className="hidden sm:flex justify-end items-center gap-8 font-semibold">
+        <a href="#" className="hover:text-gray-500">
+          Home
+        </a>
+        <a href="#" className="hover:text-gray-500">
+          About
+        </a>
+        <a href="#" className="hover:text-gray-500">
+          Contact
+        </a>
       </nav>
-    </>
+      <nav className="sm:hidden flex flex-col items-end gap-1 font-semibold">
+        <button
+          onClick={() => setShowMenu(!showMenu)}
+          className="sm:hidden font-bold text-xl hover:text-gray-500"
+        >
+          {showMenu ? <GrClose /> : <GiHamburgerMenu />}
+        </button>
+        {showMenu && (
+          <>
+            <a href="#" className="hover:text-gray-500">
+              Home
+            </a>
+            <a href="#" className="hover:text-gray-500">
+              About
+            </a>
+            <a href="#" className="hover:text-gray-500">
+              Contact
+            </a>
+          </>
+        )}
+      </nav>
+    </header>
   );
 };
 
