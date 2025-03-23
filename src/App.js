19ffbf01023e4b1330d998de0e6881fdbab3dedd
Aggelosk <akermezo@yahoo.com>
2025-03-23 14:42:19 +0200

    Add SolarSection to project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./Components/Header";
+import SolarSection from "./Components/SolarSection";
 
 function App() {
   return (
     <>
       <Header />
+      <SolarSection />
     </>
   );
 }
