c966dde7b945c521bc20a7b76c52e626ca06c528
Aggelosk <akermezo@yahoo.com>
2025-03-23 14:58:04 +0200

    delete SolarSection from project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from "react";
 import Header from "./Components/Header";
-import SolarSection from "./Components/SolarSection";
 
 function App() {
   return (
     <>
       <Header />
-      <SolarSection />
     </>
   );
 }
