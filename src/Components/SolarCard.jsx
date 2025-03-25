ac75c2243990fa8fa6faef2005bbf05d729cc399
Aggelosk <akermezo@yahoo.com>
2025-03-25 18:39:45 +0200

    SolarCard adjustments

--- src/Components/SolarCard.jsx
+++ src/Components/SolarCard.jsx
@@ -1,19 +1,23 @@
 import React from "react";
 import { Card, Button } from "react-bootstrap";
-import eurostar from "../assets/eurostar.png"
+import eurostar from "../assets/eurostar.png";
 
 const SolarCard = () => {
   return (
     <>
-      <Card style={{ width: "18rem" }}>
-        <Card.Img variant="top" src={eurostar} />
-        <Card.Body>
+      <Card className="m-4 rounded-lg border-light" style={{ width: "15rem" }}>
+        <Card.Img
+          variant="top"
+          src={eurostar}
+          style={{ width: "70%", height: "50%", margin: "0 auto" }}
+        />
+        <Card.Body className="d-flex justify-content-center flex-column">
           <Card.Title>Card Title</Card.Title>
           <Card.Text>
             Some quick example text to build on the card title and make up the
             bulk of the card's content.
           </Card.Text>
-          <Button variant="primary">Go somewhere</Button>
+          <Button variant="outline-dark">Learn More</Button>
         </Card.Body>
       </Card>
     </>
