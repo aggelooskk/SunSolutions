91602babe7d4f5b27ba4caef71ed47b1ed64fb27
Aggelosk <akermezo@yahoo.com>
2025-03-31 19:20:38 +0300

    Add image in HomeScreen

--- src/Screen/HomeScreen.jsx
+++ src/Screen/HomeScreen.jsx
@@ -4,6 +4,7 @@ import { Container } from "react-bootstrap";
 import SolarCard from "../Components/SolarCard";
 import SolarCarousel from "../Components/SolarCarousel";
 import Footer from "../Components/Footer";
+import stamps from "../assets/stamps.png";
 
 const HomeScreen = () => {
   return (
@@ -16,6 +17,9 @@ const HomeScreen = () => {
       </Container>
       <Container className="d-flex justify-content-center">
         <SolarCarousel />
+      </Container>
+      <Container className="d-flex justify-content-center m-5">
+        <img src={stamps} alt="stamps" className="w-100"></img>
       </Container>
       <Footer />
     </div>
