3c80a6c3f6debb3a674f0427fd01e5d696ee2019
Aggelosk <akermezo@yahoo.com>
2025-04-28 09:40:03 +0300

    ContactScreen progress

--- src/Screen/ContactScreen.jsx
+++ src/Screen/ContactScreen.jsx
@@ -1,7 +1,43 @@
 import React from "react";
+import Header from "../Components/Header";
+import Footer from "../Components/Footer"
+import { Form, Button, Container } from "react-bootstrap";
 
 function ContactScreen() {
-  return <div>ContactScreen</div>;
+  return (
+    <>
+    <Header />
+      <Container className="m-5">
+        <h1>Contact Us</h1>
+        <Form>
+          <Form.Group controlId="formFirstName" className="mb-3 w-25">
+            <Form.Label>First Name</Form.Label>
+            <Form.Control type="text" placeholder="Enter your first name" />
+          </Form.Group>
+
+          <Form.Group controlId="formLastName" className="mb-3 w-25">
+            <Form.Label>Last Name</Form.Label>
+            <Form.Control type="text" placeholder="Enter your last name" />
+          </Form.Group>
+
+          <Form.Group controlId="formEmail" className="mb-3 w-25">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter your email" />
+          </Form.Group>
+
+          <Form.Group controlId="formPhoneNumber" className="mb-3 w-25">
+            <Form.Label>Phone Number</Form.Label>
+            <Form.Control type="tel" placeholder="Enter your phone number" />
+          </Form.Group>
+
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </Container>
+      <Footer />
+    </>
+  );
 }
 
 export default ContactScreen;
