7b34e123c74a597f492a07b4531a7419c8c4b792
Aggelosk <akermezo@yahoo.com>
2025-06-12 11:19:20 +0300

    add responsive behavior in small screen

--- src/Screen/EcoScreen.jsx
+++ src/Screen/EcoScreen.jsx
@@ -9,117 +9,123 @@ function EcoScreen() {
       <div className="flex justify-center m-5">
         <img className="w-30 h-30" src="/eco/EcoSolars.png" />
       </div>
+      <div className="w-2/3 mx-auto md:mx-5">
+        {/* Iliothermo ECO Section */}
+        <div className="flex justify-start items-center h-10 mt-10 rounded-xl hover:bg-slate-900">
+          <h1 className="p-3 text-yellow-400 font-mono text-md">
+            Ηλιακός Θερμοσίφωνας ΗΛΙΟΘΕΡΜΟ ECO
+          </h1>
+        </div>
 
-      {/* Iliothermo ECO Section */}
-      <div className="flex justify-start items-center h-10 w-2/3 mx-5 mt-10 rounded-xl hover:bg-slate-900">
-        <h1 className="p-3 text-yellow-400 font-mono text-md">
-          Ηλιακός Θερμοσίφωνας ΗΛΙΟΘΕΡΜΟ ECO
-        </h1>
-      </div>
+        {/* Iliothermo ECO Description */}
+        <div className="mt-2 p-6 shadow-sm bg-white border-gray-100 hover:scale-105">
+          <div className="text-black space-y-4">
+            <p>
+              Οι γνωστοί ηλιακοί θερμοσίφωνες “Ηλιόθερμο ECO”, από το 1974,
+              βρίσκονται στην κορυφή των προτιμήσεων στα ηλιακά συστήματα
+              θέρμανσης ζεστού νερού χρήσης. Διατίθενται σε 80, 100, 125, 150
+              και 200 λίτρα, διπλής ή τριπλής ενέργειας.
+            </p>
+            <p>
+              Οι ηλιακοί θερμοσίφωνες “Ηλιόθερμο ECO” κατασκευάζονται με τις πιο
+              αυστηρές προδιαγραφές ποιότητας και απόδοσης.
+            </p>
+          </div>
+        </div>
 
-      {/* Iliothermo ECO Description */}
-      <div className="w-2/3 mx-5 mt-2 p-6 shadow-sm bg-white  border-gray-100 hover:scale-105">
-        <div className="text-black space-y-4">
-          <p>
-            Οι γνωστοί ηλιακοί θερμοσίφωνες “Ηλιόθερμο ECO”, από το 1974,
-            βρίσκονται στην κορυφή των προτιμήσεων στα ηλιακά συστήματα
-            θέρμανσης ζεστού νερού χρήσης. Διατίθενται σε 80, 100, 125, 150 και
-            200 λίτρα, διπλής ή τριπλής ενέργειας.
-          </p>
-          <p>
-            Οι ηλιακοί θερμοσίφωνες “Ηλιόθερμο ECO” κατασκευάζονται με τις πιο
-            αυστηρές προδιαγραφές ποιότητας και απόδοσης.
-          </p>
+        {/* Iliothermo ECO Advantages */}
+        <div className="flex justify-start items-center h-10 mt-10 rounded-xl hover:bg-slate-900">
+          <h1 className="p-3 text-yellow-400 font-mono text-md">
+            Πλεονεκτήματα
+          </h1>
+        </div>
+        <div className="mt-2 p-6 shadow-sm bg-white border-gray-100 hover:scale-105">
+          <ul className="list-disc list-inside text-black space-y-2 ml-2">
+            <li>
+              Το μόνο με ηλεκτρονικό θερμόμετρο (προαιρετικό) — Σας δείχνει
+              συνεχώς τη θερμοκρασία του νερού του ηλιακού θερμοσίφωνα μέσα στο
+              σπίτι. Έτσι σας παρέχει ΕΛΕΓΧΟ της λειτουργίας του ηλιακού σας
+              θερμοσίφωνα.
+            </li>
+            <li>
+              Οικονομία νερού — Δεν περιμένετε με το χέρι στη βρύση για να
+              φτάσει το ζεστό νερό και να ελέγξετε τη θερμοκρασία του.
+            </li>
+            <li>
+              Οικονομία ρεύματος — Ξέρετε πότε να ενεργοποιήσετε την ηλεκτρική
+              αντίσταση και για πόσο χρόνο.
+            </li>
+            <li>Ασφάλεια — Σας δείχνει τυχόν υπερθέρμανση.</li>
+          </ul>
+        </div>
+
+        {/* Iliothermo ECO Technical Specs */}
+        <div className="flex justify-start items-center h-10 mt-10 rounded-xl hover:bg-slate-900">
+          <h1 className="p-3 text-yellow-400 font-mono text-md">
+            Τεχνικά Χαρακτηριστικά
+          </h1>
+        </div>
+        <div className="mt-2 p-6 shadow-sm bg-white border-gray-100 hover:scale-105">
+          <ol className="list-decimal list-inside text-black space-y-4 ml-2">
+            <li>
+              Boiler από ευρωπαϊκό χαλυβδοέλασμα για μεγάλη διάρκεια ζωής.
+            </li>
+            <li>Πιστοποιημένο δοχείο σε πίεση 20 bar.</li>
+            <li>
+              Εσωτερική προστασία από εμαγιέ (υαλόκραμα ψημένο στους 860°C) για
+              μεγαλύτερη προστασία στα σκληρά νερά.
+            </li>
+            <li>
+              Νέο επαναστατικό εναλλάκτη κλειστού κυκλώματος που εξασφαλίζει
+              ταχύτατη θέρμανση του νερού σε πολύ υψηλή θερμοκρασία και αντοχή
+              σε 22,5 bar πίεσης.
+            </li>
+            <li>
+              Ισχυρή μόνωση χυτής οικολογικής πολυουρεθάνης 5 εκ., πυκνότητας
+              50kg/m³ που εξασφαλίζει διατήρηση ζεστού νερού ακόμα και τις πιο
+              παγωμένες νύχτες του χειμώνα.
+            </li>
+            <li>
+              Ράβδο μαγνησίου για ανοδική προστασία κατά της ηλεκτρόλυσης.
+            </li>
+            <li>
+              Ηλεκτρική αντίσταση με θερμοστάτη για ζεστό νερό ακόμη και με
+              συννεφιά.
+            </li>
+            <li>
+              Πλαίσιο συλλέκτη από προφίλ αλουμινίου με προστασία από
+              ηλεκτροστατική βαφή φούρνου για απεριόριστη διάρκεια ζωής.
+            </li>
+            <li>
+              Απορροφητή από χάλκινο υδροσκελετό συγκολλημένο με laser σε
+              επιλεκτική επιφάνεια αλουμινίου. Απορροφητικότητα 95%,
+              εκπεμψιμότητα 5%.
+            </li>
+            <li>Ο μόνος πιστοποιημένος συλλέκτης σε αντοχή 22,5 bar.</li>
+            <li>
+              Υψηλή απόδοση, λόγω ισχυρής μόνωσης πετροβάμβακα στην πλάτη και
+              στα πλευρικά τοιχώματα που περιορίζει τις απώλειες θερμότητας από
+              τον ηλιακό συλλέκτη.
+            </li>
+          </ol>
+        </div>
+
+        <div className="flex justify-start my-7">
+          <img src="/eco/EcoSpecs.png" alt="" />
+        </div>
+
+        {/* Iliothermo ECO Sizes */}
+        <div className="flex justify-start items-center h-10 mt-10 rounded-xl hover:bg-slate-900">
+          <h1 className="p-3 text-yellow-400 font-mono text-md">Διαστάσεις</h1>
+        </div>
+        <div className="flex justify-start my-5">
+          <img src="/eco/EcoSizes.png" alt="" />
+        </div>
+
+        <div className="flex justify-start my-7">
+          <img src="/eco/EcoMod.png" alt="" />
         </div>
       </div>
-
-      {/* Iliothermo ECO Advantages */}
-      <div className="flex justify-start items-center h-10 w-2/3 mx-5 mt-10 rounded-xl hover:bg-slate-900">
-        <h1 className="p-3 text-yellow-400 font-mono text-md">Πλεονεκτήματα</h1>
-      </div>
-      <div className="w-2/3 mx-5 mt-2 p-6 shadow-sm bg-white  border-gray-100 hover:scale-105">
-        <ul className="list-disc list-inside text-black space-y-2 ml-2">
-          <li>
-            Το μόνο με ηλεκτρονικό θερμόμετρο (προαιρετικό) — Σας δείχνει
-            συνεχώς τη θερμοκρασία του νερού του ηλιακού θερμοσίφωνα μέσα στο
-            σπίτι. Έτσι σας παρέχει ΕΛΕΓΧΟ της λειτουργίας του ηλιακού σας
-            θερμοσίφωνα.
-          </li>
-          <li>
-            Οικονομία νερού — Δεν περιμένετε με το χέρι στη βρύση για να φτάσει
-            το ζεστό νερό και να ελέγξετε τη θερμοκρασία του.
-          </li>
-          <li>
-            Οικονομία ρεύματος — Ξέρετε πότε να ενεργοποιήσετε την ηλεκτρική
-            αντίσταση και για πόσο χρόνο.
-          </li>
-          <li>Ασφάλεια — Σας δείχνει τυχόν υπερθέρμανση.</li>
-        </ul>
-      </div>
-
-      {/* Iliothermo ECO Technical Specs */}
-      <div className="flex justify-start items-center h-10 w-2/3 mx-5 mt-10 rounded-xl hover:bg-slate-900">
-        <h1 className="p-3 text-yellow-400 font-mono text-md">
-          Τεχνικά Χαρακτηριστικά
-        </h1>
-      </div>
-      <div className="w-2/3 mx-5 mt-2 p-6 shadow-sm bg-white  border-gray-100 hover:scale-105">
-        <ol className="list-decimal list-inside text-black space-y-4 ml-2">
-          <li>Boiler από ευρωπαϊκό χαλυβδοέλασμα για μεγάλη διάρκεια ζωής.</li>
-          <li>Πιστοποιημένο δοχείο σε πίεση 20 bar.</li>
-          <li>
-            Εσωτερική προστασία από εμαγιέ (υαλόκραμα ψημένο στους 860°C) για
-            μεγαλύτερη προστασία στα σκληρά νερά.
-          </li>
-          <li>
-            Νέο επαναστατικό εναλλάκτη κλειστού κυκλώματος που εξασφαλίζει
-            ταχύτατη θέρμανση του νερού σε πολύ υψηλή θερμοκρασία και αντοχή σε
-            22,5 bar πίεσης.
-          </li>
-          <li>
-            Ισχυρή μόνωση χυτής οικολογικής πολυουρεθάνης 5 εκ., πυκνότητας
-            50kg/m³ που εξασφαλίζει διατήρηση ζεστού νερού ακόμα και τις πιο
-            παγωμένες νύχτες του χειμώνα.
-          </li>
-          <li>Ράβδο μαγνησίου για ανοδική προστασία κατά της ηλεκτρόλυσης.</li>
-          <li>
-            Ηλεκτρική αντίσταση με θερμοστάτη για ζεστό νερό ακόμη και με
-            συννεφιά.
-          </li>
-          <li>
-            Πλαίσιο συλλέκτη από προφίλ αλουμινίου με προστασία από
-            ηλεκτροστατική βαφή φούρνου για απεριόριστη διάρκεια ζωής.
-          </li>
-          <li>
-            Απορροφητή από χάλκινο υδροσκελετό συγκολλημένο με laser σε
-            επιλεκτική επιφάνεια αλουμινίου. Απορροφητικότητα 95%, εκπεμψιμότητα
-            5%.
-          </li>
-          <li>Ο μόνος πιστοποιημένος συλλέκτης σε αντοχή 22,5 bar.</li>
-          <li>
-            Υψηλή απόδοση, λόγω ισχυρής μόνωσης πετροβάμβακα στην πλάτη και στα
-            πλευρικά τοιχώματα που περιορίζει τις απώλειες θερμότητας από τον
-            ηλιακό συλλέκτη.
-          </li>
-        </ol>
-      </div>
-
-      <div className="flex justify-start w-2/3 mx-5 my-7">
-        <img src="/eco/EcoSpecs.png" alt="" />
-      </div>
-
-      {/* Iliothermo ECO Sizes */}
-      <div className="flex justify-start items-center h-10 w-2/3 mx-5 mt-10 rounded-xl hover:bg-slate-900">
-        <h1 className="p-3 text-yellow-400 font-mono text-md">Διαστάσεις</h1>
-      </div>
-      <div className="flex justify-start w-2/3 mx-5 my-5">
-        <img src="/eco/EcoSizes.png" alt="" />
-      </div>
-
-      <div className="flex justify-start w-2/3 mx-5 my-7">
-        <img src="/eco/EcoMod.png" alt="" />
-      </div>
-
       <Footer />
     </>
   );
--- src/Screen/FinoScreen.jsx
+++ src/Screen/FinoScreen.jsx
@@ -5,117 +5,121 @@ function FinoScreen() {
   return (
     <>
       <NavBar />
+      <div className="w-2/3 mx-auto md:mx-5">
+        {/* Header Section */}
+        <div className="flex justify-start items-center h-10 mt-10 rounded-xl hover:bg-slate-900">
+          <h1 className="p-3 text-yellow-400 font-mono text-md">
+            Ηλιακοί Θερμοσίφωνες Χαμηλού Ύψους
+          </h1>
+        </div>
 
-      {/* Header Section */}
-      <div className="flex justify-start items-center h-10 w-2/3 mx-5 mt-10 rounded-xl hover:bg-slate-900">
-        <h1 className="p-3 text-yellow-400 font-mono text-md">
-          Ηλιακοί Θερμοσίφωνες Χαμηλού Ύψους
-        </h1>
-      </div>
+        {/* Description */}
+        <div className="mt-2 p-6 shadow-sm bg-white border-gray-100 hover:scale-105">
+          <div className="text-black space-y-4">
+            <li>
+              Ο ηλιακός θερμοσίφωνας EUROSTAR FINO έχει σχεδιαστεί και
+              κατασκευαστεί με κύριο στόχο να εξασφαλίζει μεγάλη απόδοση,
+              διάρκεια ζωής και διακριτική εμφάνιση.
+            </li>
+            <li>
+              Ο νέος χάλκινος επιλεκτικός συλλέκτης τιτανίου με τον υψηλότερο
+              βαθμό απόδοσης, παρέχει συνέχεια καυτό νερό ακόμα και στις
+              δυσκολότερες καιρικές συνθήκες. Τα προηγμένα υλικά που
+              χρησιμοποιούνται στην κατασκευή του και η προσοχή σε κάθε
+              λεπτομέρεια, εξασφαλίζουν την άψογη λειτουργία του για μια
+              ολόκληρη ζωή.
+            </li>
+            <li>
+              Οι ηλιακοί θερμοσίφωνες EUROSTAR FINO κατασκευάζονται σύμφωνα με
+              το διεθνές πρότυπο διασφάλισης ποιότητας ISO 9001-2015 και
+              ελέγχονται από το διεθνές πρότυπο ISO/DP9459-2.
+            </li>
+          </div>
+        </div>
 
-      {/* Description */}
-      <div className="w-2/3 mx-5 mt-2 p-6 shadow-sm bg-white  border-gray-100 hover:scale-105">
-        <div className="text-black space-y-4">
-          <li>
-            Ο ηλιακός θερμοσίφωνας EUROSTAR FINO έχει σχεδιαστεί και
-            κατασκευαστεί με κύριο στόχο να εξασφαλίζει μεγάλη απόδοση, διάρκεια
-            ζωής και διακριτική εμφάνιση.
-          </li>
-          <li>
-            Ο νέος χάλκινος επιλεκτικός συλλέκτης τιτανίου με τον υψηλότερο
-            βαθμό απόδοσης, παρέχει συνέχεια καυτό νερό ακόμα και στις
-            δυσκολότερες καιρικές συνθήκες. Τα προηγμένα υλικά που
-            χρησιμοποιούνται στην κατασκευή του και η προσοχή σε κάθε
-            λεπτομέρεια, εξασφαλίζουν την άψογη λειτουργία του για μια ολόκληρη
-            ζωή.
-          </li>
-          <li>
-            Οι ηλιακοί θερμοσίφωνες EUROSTAR FINO κατασκευάζονται σύμφωνα με το
-            διεθνές πρότυπο διασφάλισης ποιότητας ISO 9001-2015 και ελέγχονται
-            από το διεθνές πρότυπο ISO/DP9459-2.
-          </li>
+        {/* Boiler Technical Specs */}
+        <div className="flex justify-start items-center h-10 mt-10 rounded-xl hover:bg-slate-900">
+          <h1 className="p-3 text-yellow-400 font-mono text-md">
+            Τεχνικά Χαρακτηριστικά Boiler
+          </h1>
+        </div>
+        <div className="mt-2 p-6 shadow-sm bg-white border-gray-100 hover:scale-105">
+          <ol className="list-decimal list-inside text-black space-y-4 ml-2">
+            <li>
+              Εξωτερικό περίβλημα από ανοξείδωτο ατσάλι ΙΝΟΧ ή χάλυβα
+              ηλεκτροστατικής βαφής φούρνου για απεριόριστη διάρκεια ζωής με
+              μέγιστο ύψος 1μ.
+            </li>
+            <li>
+              Ισχυρή μόνωση χυτής οικολογικής πολυουρεθάνης 5–7 cm, πυκνότητας
+              50 kg/m³ με θερμομονωτικό συντελεστή 0.019 kcal/m/h/°C.
+            </li>
+            <li>
+              Μανδύας κλειστού κυκλώματος για μέγιστη επιφάνεια εναλλαγής
+              θερμότητας και ταχύτερη θέρμανση νερού.
+            </li>
+            <li>
+              Boiler από ευρωπαϊκό χαλυβδοέλασμα με εσωτερική προστασία από
+              εμαγιέ (860°C) για προστασία από σκληρά νερά.
+            </li>
+            <li>Ανοδική προστασία με ράβδο μαγνησίου κατά της ηλεκτρόλυσης.</li>
+            <li>
+              Ηλεκτρική αντίσταση 4kW για ζεστό νερό ακόμα και με συννεφιά.
+            </li>
+            <li>
+              Θερμοστάτης ασφαλείας διπλής επαφής με θερμικό ασφαλείας για
+              προστασία από υπερθέρμανση.
+            </li>
+          </ol>
+        </div>
+
+        {/* Advantages */}
+        <div className="flex justify-start items-center h-10 mt-10 rounded-xl hover:bg-slate-900">
+          <h1 className="p-3 text-yellow-400 font-mono text-md">
+            Πλεονεκτήματα
+          </h1>
+        </div>
+        <div className="mt-2 p-6 shadow-sm bg-white border-gray-100 hover:scale-105">
+          <ul className="list-disc list-inside text-black space-y-2 ml-2">
+            <li>Απλή εγκατάσταση σε επίπεδη σκεπή σε ελάχιστο χρόνο.</li>
+            <li>Συνδέεται και με Καλοριφέρ (προαιρετικά).</li>
+          </ul>
+        </div>
+
+        {/* Collector Technical Specs */}
+        <div className="flex justify-start items-center h-10 mt-10 rounded-xl hover:bg-slate-900">
+          <h1 className="p-3 text-yellow-400 font-mono text-md">
+            Τεχνικά Χαρακτηριστικά Ηλιακού Συλλέκτη
+          </h1>
+        </div>
+        <div className="mt-2 p-6 shadow-sm bg-white border-gray-100 hover:scale-105">
+          <ol className="list-decimal list-inside text-black space-y-4 ml-2">
+            <li>
+              Πλαίσιο από χυτό προφίλ αλουμινίου με ηλεκτροστατική βαφή, χωρίς
+              ραφές και βίδες για πλήρη στεγανότητα.
+            </li>
+            <li>
+              Επιλεκτικός χάλκινος υδροσκελετός (Τιτανίου-μπλε) με φύλλα
+              αλουμινίου για υψηλή απορροφητικότητα.
+            </li>
+            <li>
+              Μόνωση με πετροβάμβακα 40 mm στην πλάτη και 20 mm στα πλαϊνά για
+              ελαχιστοποίηση θερμικών απωλειών.
+            </li>
+            <li>
+              Πρισματικό άθραυστο κρύσταλλο securit 4mm ψημένο στους 700ºC, με
+              90,5% διαπερατότητα.
+            </li>
+            <li>
+              Ελαστικά EPDM και σιλικονούχα παρεμβύσματα για αντοχή έως 160 ºC.
+            </li>
+            <li>
+              Θερμικό υγρό προπυλενογλυκόλης με αντιδιαβρωτικά για ταχεία
+              θέρμανση και αντοχή σε παγετό και οξείδωση.
+            </li>
+          </ol>
         </div>
       </div>
-
-      {/* Boiler Technical Specs */}
-      <div className="flex justify-start items-center h-10 w-2/3 mx-5 mt-10 rounded-xl hover:bg-slate-900">
-        <h1 className="p-3 text-yellow-400 font-mono text-md">
-          Τεχνικά Χαρακτηριστικά Boiler
-        </h1>
-      </div>
-      <div className="w-2/3 mx-5 mt-2 p-6 shadow-sm bg-white  border-gray-100 hover:scale-105">
-        <ol className="list-decimal list-inside text-black space-y-4 ml-2">
-          <li>
-            Εξωτερικό περίβλημα από ανοξείδωτο ατσάλι ΙΝΟΧ ή χάλυβα
-            ηλεκτροστατικής βαφής φούρνου για απεριόριστη διάρκεια ζωής με
-            μέγιστο ύψος 1μ.
-          </li>
-          <li>
-            Ισχυρή μόνωση χυτής οικολογικής πολυουρεθάνης 5–7 cm, πυκνότητας 50
-            kg/m³ με θερμομονωτικό συντελεστή 0.019 kcal/m/h/°C.
-          </li>
-          <li>
-            Μανδύας κλειστού κυκλώματος για μέγιστη επιφάνεια εναλλαγής
-            θερμότητας και ταχύτερη θέρμανση νερού.
-          </li>
-          <li>
-            Boiler από ευρωπαϊκό χαλυβδοέλασμα με εσωτερική προστασία από εμαγιέ
-            (860°C) για προστασία από σκληρά νερά.
-          </li>
-          <li>Ανοδική προστασία με ράβδο μαγνησίου κατά της ηλεκτρόλυσης.</li>
-          <li>Ηλεκτρική αντίσταση 4kW για ζεστό νερό ακόμα και με συννεφιά.</li>
-          <li>
-            Θερμοστάτης ασφαλείας διπλής επαφής με θερμικό ασφαλείας για
-            προστασία από υπερθέρμανση.
-          </li>
-        </ol>
-      </div>
-
-      {/* Advantages */}
-      <div className="flex justify-start items-center h-10 w-2/3 mx-5 mt-10 rounded-xl hover:bg-slate-900">
-        <h1 className="p-3 text-yellow-400 font-mono text-md">Πλεονεκτήματα</h1>
-      </div>
-      <div className="w-2/3 mx-5 mt-2 p-6 shadow-sm bg-white  border-gray-100 hover:scale-105">
-        <ul className="list-disc list-inside text-black space-y-2 ml-2">
-          <li>Απλή εγκατάσταση σε επίπεδη σκεπή σε ελάχιστο χρόνο.</li>
-          <li>Συνδέεται και με Καλοριφέρ (προαιρετικά).</li>
-        </ul>
-      </div>
-
-      {/* Collector Technical Specs */}
-      <div className="flex justify-start items-center h-10 w-2/3 mx-5 mt-10 rounded-xl hover:bg-slate-900">
-        <h1 className="p-3 text-yellow-400 font-mono text-md">
-          Τεχνικά Χαρακτηριστικά Ηλιακού Συλλέκτη
-        </h1>
-      </div>
-      <div className="w-2/3 mx-5 mt-2 p-6 shadow-sm bg-white  border-gray-100 hover:scale-105">
-        <ol className="list-decimal list-inside text-black space-y-4 ml-2">
-          <li>
-            Πλαίσιο από χυτό προφίλ αλουμινίου με ηλεκτροστατική βαφή, χωρίς
-            ραφές και βίδες για πλήρη στεγανότητα.
-          </li>
-          <li>
-            Επιλεκτικός χάλκινος υδροσκελετός (Τιτανίου-μπλε) με φύλλα
-            αλουμινίου για υψηλή απορροφητικότητα.
-          </li>
-          <li>
-            Μόνωση με πετροβάμβακα 40 mm στην πλάτη και 20 mm στα πλαϊνά για
-            ελαχιστοποίηση θερμικών απωλειών.
-          </li>
-          <li>
-            Πρισματικό άθραυστο κρύσταλλο securit 4mm ψημένο στους 700ºC, με
-            90,5% διαπερατότητα.
-          </li>
-          <li>
-            Ελαστικά EPDM και σιλικονούχα παρεμβύσματα για αντοχή έως 160 ºC.
-          </li>
-          <li>
-            Θερμικό υγρό προπυλενογλυκόλης με αντιδιαβρωτικά για ταχεία θέρμανση
-            και αντοχή σε παγετό και οξείδωση.
-          </li>
-        </ol>
-      </div>
-
       <Footer />
     </>
   );
