cd5f518f168219e1729b04fc6113508bce19a757
Aggelosk <akermezo@yahoo.com>
2025-06-28 11:52:47 +0300

    fix imports in carousel.js

--- src/carousel.js
+++ src/carousel.js
@@ -1,8 +1,8 @@
-import img6832 from "./Images/carousel/IMG_6832.JPEG"
-import img5492 from "./Images/carousel/IMG_5492.JPEG";
-import img6830 from "./Images/carousel/IMG_6830.JPEG";
-import img8573 from "./Images/carousel/IMG_8573.JPEG";
-import img8575 from "./Images/carousel/IMG_8575.JPEG";
+import img6832 from "./Images/carousel/IMG_6832.jpeg"
+import img5492 from "./Images/carousel/IMG_5492.jpeg";
+import img6830 from "./Images/carousel/IMG_6830.jpeg";
+import img8573 from "./Images/carousel/IMG_8573.jpeg";
+import img8575 from "./Images/carousel/IMG_8575.jpeg";
 
 const carouselData = [
   { src: img6832 },
