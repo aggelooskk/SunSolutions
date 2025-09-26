b5303ad15048ed64ec4eef4c67c267031ec1563e
Aggelosk <akermezo@yahoo.com>
2025-09-26 12:44:29 +0300

    Add images to carousel

--- src/carousel.js
+++ src/carousel.js
@@ -1,8 +1,10 @@
-import img6832 from "./Images/carousel/IMG_6832.jpeg"
+import img6832 from "./Images/carousel/IMG_6832.jpeg";
 import img5492 from "./Images/carousel/IMG_5492.jpeg";
 import img6830 from "./Images/carousel/IMG_6830.jpeg";
 import img8573 from "./Images/carousel/IMG_8573.jpeg";
 import img8575 from "./Images/carousel/IMG_8575.jpeg";
+import img8784 from "./Images/carousel/img_8784.jpeg";
+import img8788 from "./Images/carousel/img_8788.jpeg";
 
 const carouselData = [
   { src: img6832 },
@@ -10,6 +12,8 @@ const carouselData = [
   { src: img6830 },
   { src: img8573 },
   { src: img8575 },
+  { src: img8784 },
+  { src: img8788 },
 ];
 
 export default carouselData;
