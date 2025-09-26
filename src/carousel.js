ef8cba8830cb8f34423f697a32e55a6fdd086b0d
Aggelosk <akermezo@yahoo.com>
2025-09-26 12:48:40 +0300

    Typo fix

--- src/carousel.js
+++ src/carousel.js
@@ -3,8 +3,8 @@ import img5492 from "./Images/carousel/IMG_5492.jpeg";
 import img6830 from "./Images/carousel/IMG_6830.jpeg";
 import img8573 from "./Images/carousel/IMG_8573.jpeg";
 import img8575 from "./Images/carousel/IMG_8575.jpeg";
-import img8784 from "./Images/carousel/img_8784.jpeg";
-import img8788 from "./Images/carousel/img_8788.jpeg";
+import img8784 from "./Images/carousel/IMG_8784.jpeg";
+import img8788 from "./Images/carousel/IMG_8788.jpeg";
 
 const carouselData = [
   { src: img6832 },
